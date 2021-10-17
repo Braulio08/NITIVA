0c146c1a3f389648834487b3c1ad8b7a2a689399
Braulio <brabs108@gmail.com>
2021-10-17 14:33:06 -0600

    Actualización 17/10/2021 14:32

--- app/src/main/java/com/brafa/nitiva/MemoryGame.java
+++ app/src/main/java/com/brafa/nitiva/MemoryGame.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class MemoryGame extends AppCompatActivity {
 
@@ -31,7 +34,7 @@ public class MemoryGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_game);
-        cargarBotones();
+        iniciar();
     }
     private void cargarMatriz(){
         imb00 = findViewById(R.id.btn00);
@@ -47,9 +50,53 @@ public class MemoryGame extends AppCompatActivity {
         imb10 = findViewById(R.id.btn10);
         imb11 = findViewById(R.id.btn11);
 
+        matriz[0] = imb00;
+        matriz[1] = imb01;
+        matriz[2] = imb02;
+        matriz[3] = imb03;
+        matriz[4] = imb04;
+        matriz[5] = imb05;
+        matriz[6] = imb06;
+        matriz[7] = imb07;
+        matriz[8] = imb08;
+        matriz[9] = imb09;
+        matriz[10] = imb10;
+        matriz[11] = imb11;
+
+    }
+    public void cargarTexto(){
+        puntajePantalla = findViewById(R.id.textViewPuntaje);
+        puntaje=0;
+        aciertos=0;
+        puntajePantalla.setText(puntaje);
+    }
+    public void cargarImagenes(){
+        imagenes = new int[]{
+                R.drawable.bee,
+                R.drawable.cherry,
+                R.drawable.cow,
+                R.drawable.flower,
+                R.drawable.grapes,
+                R.drawable.hen,
+                R.drawable.leaf,
+                R.drawable.parrot,
+                R.drawable.pawprint,
+                R.drawable.pig,
+                R.drawable.rose,
+                R.drawable.strawberry,
+                R.drawable.sunny
+        };
+        fondo = R.drawable.background;
     }
     public void cargarBotones(){
         salir = findViewById(R.id.imageButtonSalir);
+        reiniciar = findViewById(R.id.imageButtonReiniciar);
+        reiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iniciar();
+            }
+        });
         salir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,5 +105,24 @@ public class MemoryGame extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private ArrayList<Integer> barajar(int longitud){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < longitud*2; i++) {
+            result.add(i%longitud);
+        }
+        Collections.shuffle(result);
+        return result;
+    }
+    public void iniciar(){
+        cargarMatriz();
+        cargarBotones();
+        cargarTexto();
+        cargarImagenes();
+        arrayCaos = barajar(imagenes.length);
+        for (int i = 0; i < matriz.length; i++) {
+            matriz[i].setScaleType(ImageView.ScaleType.FIT_CENTER);
+            matriz[i].setImageResource(imagenes[arrayCaos.get(i)]);
+        }
     }
 }
