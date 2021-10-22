bc37b4d995deb4c4fbb4fa131958fc990f14bf9a
Braulio <brabs108@gmail.com>
2021-10-21 21:25:38 -0600

    Actualización 21/10/2021 21:25 Archivos Hangman

--- app/src/main/java/com/brafa/nitiva/HangmanGame.java
+++ app/src/main/java/com/brafa/nitiva/HangmanGame.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class HangmanGame extends AppCompatActivity {
 
+    private String [] palabras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
