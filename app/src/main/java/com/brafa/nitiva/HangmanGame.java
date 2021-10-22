7dab1986ca25faa5f496772b135ec6decf679d96
Braulio <brabs108@gmail.com>
2021-10-21 21:15:07 -0600

    Actualización 21/10/2021 21:14 Archivos Hangman

---
+++ app/src/main/java/com/brafa/nitiva/HangmanGame.java
@@ -0,0 +1,14 @@
+package com.brafa.nitiva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HangmanGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangman_game);
+    }
+}
