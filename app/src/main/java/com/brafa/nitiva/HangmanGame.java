e1e851869ed7a9d174970e8ef0e65c320b3cb2fa
Braulio <brabs108@gmail.com>
2021-10-24 15:52:01 -0600

    Actualización 22/10/2021 15:51

--- app/src/main/java/com/brafa/nitiva/HangmanGame.java
+++ app/src/main/java/com/brafa/nitiva/HangmanGame.java
@@ -11,6 +11,7 @@ import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class HangmanGame extends AppCompatActivity {
@@ -21,7 +22,9 @@ public class HangmanGame extends AppCompatActivity {
     private TextView [] charView;
     private LinearLayout palabraLayout;
     private LetterAdapter adapter;
-    private GridView gridView;
+    private WordAdapter wordAdapter;
+    private GridView gridView, wordView;
+    ArrayList<String> word = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +32,22 @@ public class HangmanGame extends AppCompatActivity {
         palabras = getResources().getStringArray(R.array.palabras);
         palabraLayout = findViewById(R.id.LinearCharts);
         gridView = findViewById(R.id.screenLetters);
+        wordView = findViewById(R.id.screenWord);
         play();
     }
     private void play(){
+
         String nPalabra = palabras[random.nextInt(palabras.length)];
+        while (nPalabra.equals(actual))nPalabra = palabras[random.nextInt(palabras.length)];
+
+        actual = nPalabra;
+
+        for (int i = 0; i < actual.length(); i++) {
+            word.add(""+actual.charAt(i));
+        }
+
+
+        /*String nPalabra = palabras[random.nextInt(palabras.length)];
         while (nPalabra.equals(actual))nPalabra = palabras[random.nextInt(palabras.length)];
 
         actual = nPalabra;
@@ -42,16 +57,23 @@ public class HangmanGame extends AppCompatActivity {
         for (int i = 0; i < actual.length(); i++) {
             charView[i] = new TextView(this);
             charView[i].setText(""+actual.charAt(i));
-            charView[i].setLayoutParams(new ViewGroup.LayoutParams(130, ViewGroup.LayoutParams.WRAP_CONTENT));
+            charView[i].setLayoutParams(new ViewGroup.LayoutParams(130, ViewGroup.LayoutParams.MATCH_PARENT));
             charView[i].setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-
             charView[i].setTextColor(Color.BLACK);
             charView[i].setTextSize(75);
             charView[i].setBackgroundResource(R.drawable.charshape);
             palabraLayout.addView(charView[i]);
-        }
+        }*/
+
+
+
+
         adapter = new LetterAdapter(this);
         gridView.setAdapter(adapter);
+
+        wordAdapter = new WordAdapter( word, this);
+        wordView.setAdapter(wordAdapter);
+
     }
 
 }
