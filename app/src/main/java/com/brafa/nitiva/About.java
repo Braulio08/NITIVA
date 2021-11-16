cbfb026f671f1df6e8755e14940374cfd0984b2a
Braulio <brabs108@gmail.com>
2021-11-16 14:47:59 -0600

    Actualización 16/11/2021 Creación de fragmentos para cada sección.

--- app/src/main/java/com/brafa/nitiva/About.java
+++ app/src/main/java/com/brafa/nitiva/About.java
@@ -2,6 +2,7 @@ package com.brafa.nitiva;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class About extends AppCompatActivity {
     Button btnCerrar;
     CardView cvBotones, cvAlzheimer, cvAcercaDe;
     LinearLayout linearLayout;
+    FragmentContainerView fragmentContainerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +30,53 @@ public class About extends AppCompatActivity {
         cvBotones = findViewById(R.id.cvBotones);
         cvAlzheimer = findViewById(R.id.cvAlzheimer);
 
+        fragmentContainerView = findViewById(R.id.fragmentContainerView);
+
         btnCerrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(About.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+
+                if(fragmentContainerView.getVisibility()==View.VISIBLE)
+                {
+                    fragmentContainerView.setVisibility(View.GONE);
+                    cvAcercaDe.setVisibility(View.VISIBLE);
+                    cvAlzheimer.setVisibility(View.VISIBLE);
+                    cvBotones.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    Intent intent = new Intent(About.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
 
         cvAlzheimer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(linearLayout,"En mantenimiento",Snackbar.LENGTH_SHORT).show();
+                fragmentContainerView.setVisibility(View.VISIBLE);
+                cvAcercaDe.setVisibility(View.GONE);
+                cvAlzheimer.setVisibility(View.GONE);
+                cvBotones.setVisibility(View.GONE);
             }
         });
         cvBotones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(linearLayout,"En mantenimiento",Snackbar.LENGTH_SHORT).show();
+                fragmentContainerView.setVisibility(View.VISIBLE);
+                cvAcercaDe.setVisibility(View.GONE);
+                cvAlzheimer.setVisibility(View.GONE);
+                cvBotones.setVisibility(View.GONE);
             }
         });
         cvAcercaDe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(linearLayout,"En mantenimiento",Snackbar.LENGTH_SHORT).show();
+                fragmentContainerView.setVisibility(View.VISIBLE);
+                cvAcercaDe.setVisibility(View.GONE);
+                cvAlzheimer.setVisibility(View.GONE);
+                cvBotones.setVisibility(View.GONE);
             }
         });
     }
